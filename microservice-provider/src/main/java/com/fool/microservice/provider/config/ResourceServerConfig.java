ead7425be47facfe1608b84a249d955b3846f158
80010379 <linzheng.shen@hxgroup.com>
2021-11-16 16:16:37 +0800

    gitignore

--- microservice-provider/src/main/java/com/fool/microservice/provider/config/ResourceServerConfig.java
+++ microservice-provider/src/main/java/com/fool/microservice/provider/config/ResourceServerConfig.java
@@ -1,13 +1,19 @@
 package com.fool.microservice.provider.config;
 
+import com.alibaba.fastjson.JSON;
 import com.fool.microservice.provider.customize.CustomizeAccessDecisionManager;
 import com.fool.microservice.provider.customize.CustomizeFilterInvocationSecurityMetadataSource;
+import com.fool.microservice.provider.entity.DefaultResult;
+import com.fool.microservice.provider.entity.Result;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
@@ -43,13 +49,39 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         return o;
                     }
                 })
-                .anyRequest().authenticated();
-
-        // http.csrf().disable()
-        //         .authorizeRequests()
-        //         .antMatchers("/auth-test").hasAnyAuthority("!ADMIN")
-        //         .antMatchers("/auth-test2").hasAnyAuthority("ADMIN")
-        //         .antMatchers("/**").permitAll()
-        //         .anyRequest().authenticated();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                //权限拒绝处理逻辑
+                .accessDeniedHandler(accessDeniedHandler())
+                //匿名用户访问无权限资源时的异常处理
+                .authenticationEntryPoint(authenticationEntryPoint());
     }
+
+    /**
+     * 访问被拒
+     */
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            DefaultResult<?> result = new DefaultResult<>(Result.ACCESS_DENIED,"access denied",null);
+            httpServletResponse.setContentType("text/json;charset=utf-8");
+            httpServletResponse.getWriter().write(JSON.toJSONString(result));
+
+        };
+    }
+
+    /**
+     * 未登录
+     */
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (httpServletRequest, httpServletResponse, e) -> {
+            DefaultResult<?> result = new DefaultResult<>(Result.AUTH_FAILED,"Full authentication is required to access this resource",null);
+            httpServletResponse.setContentType("text/json;charset=utf-8");
+            httpServletResponse.getWriter().write(JSON.toJSONString(result));
+        };
+    }
+
 }
